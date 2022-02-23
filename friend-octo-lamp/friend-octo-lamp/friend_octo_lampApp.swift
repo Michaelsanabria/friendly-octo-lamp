3229908398f38cdceecd93b4b77404e15dad0e8a
Michaelsanabria <sanabria.michael@icloud.com>
2022-02-23 18:08:29 -0500

    Create friend_octo_lampApp.swift

---
+++ friend-octo-lamp/friend-octo-lamp/friend_octo_lampApp.swift
@@ -0,0 +1,17 @@
+//
+//  friend_octo_lampApp.swift
+//  friend-octo-lamp
+//
+//  Created by Michael Sanabria on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct friend_octo_lampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
